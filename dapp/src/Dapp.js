c8476e0e13bd4c5c15a73630fe9f509a9796a3ea
n1c01a5 <contact@wagner-nicolas.com>
2016-12-16 17:48:31 +0100

    fix dependency library sha3

--- dapp/src/Dapp.js
+++ dapp/src/Dapp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import FontAwesome from 'react-fontawesome'
 import GithubCorner from 'react-github-corner';
+import { keccak_256 } from 'js-sha3';
 import { Button, Jumbotron, Navbar, NavbarBrand, Nav, NavItem, NavLink, Tooltip, TooltipContent, Container, Row, Col } from 'reactstrap';
 
 import 'styles/App.scss'
@@ -48,7 +49,7 @@ class Dapp extends Component {
   isChecksumAddress = (address) => {
       // Check each case
       address = address.replace('0x','');
-      let addressHash = sha3(address.toLowerCase());
+      let addressHash = keccak_256(address.toLowerCase());
       for (var i = 0; i < 40; i++ ) {
           // the nth letter should be uppercase if the nth digit of casemap is 1
           if ((parseInt(addressHash[i], 16) > 7 && address[i].toUpperCase() !== address[i]) || (parseInt(addressHash[i], 16) <= 7 && address[i].toLowerCase() !== address[i])) {
