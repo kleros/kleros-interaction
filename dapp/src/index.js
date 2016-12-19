78be34685889c9a34cc7e2d4f1ed1792ce6acde8
n1c01a5 <contact@wagner-nicolas.com>
2016-12-19 19:14:47 +0100

    add route ExampleArbitreCOntract

--- dapp/src/index.js
+++ dapp/src/index.js
@@ -4,11 +4,13 @@ import { Router, Route, hashHistory } from 'react-router'
 import App from './App';
 import Docs from './Docs';
 import Dapp from './Dapp';
+import ExampleArbitrableContract from './components/ExampleArbitrableContract';
 
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={App}/>
     <Route path="/docs" component={Docs}/>
     <Route path="/dapp" component={Dapp}/>
+    <Route path="/examplearbitrable/:contractAdress" component={ExampleArbitrableContract}/>
   </Router>
 ), document.getElementById('root'))
