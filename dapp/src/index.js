645e77c5d5ce68ec1e1af2acd204732935449c87
n1c01a5 <contact@wagner-nicolas.com>
2017-02-09 20:20:41 +0100

    fix import

--- dapp/src/index.js
+++ dapp/src/index.js
@@ -8,7 +8,6 @@ import ExampleArbitrableContract from './components/ExampleArbitrableContract'
 ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={App}/>
-    <Route path="/docs" component={Docs}/>
     <Route path="/dapp" component={Dapp}/>
     <Route path="/examplearbitrable/:contractAdress" component={ExampleArbitrableContract}/>
   </Router>
