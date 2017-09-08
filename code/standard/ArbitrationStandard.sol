472bbd00c92789efa3840811f44c253bf58e581b
Clément Lesaege <clemage@live.fr>
2017-09-08 21:36:16 +0200

    Comments + executeRuling.
    Added comments and put ruling execute in an abstract function

--- code/standard/ArbitrationStandard.sol
+++ code/standard/ArbitrationStandard.sol
@@ -1,25 +1,65 @@
 /**
  *  @title Arbitration Standard
  *  @author Clément Lesaege - <clement@lesaege.com>
+ *  Bug Bounties: This code hasn't undertaken a bug bounty program yet.
  */
 
 pragma solidity ^0.4.13;
 
+/** @title Arbitrable
+ *  Arbitrable abstract contract.
+ *  When developing arbitrable contracts, we need to:
+ *  -Define the action taken when a ruling is received by the contract. We should do so in executeRuling.
+ *  -Allow dispute creation. For this a function must:
+ *      -Call arbitrator.createDispute.value(_fee)(_choices,_extraData);
+ *      -Create the event Dispute(_arbitrator,_disputeID,_rulingOptions);
+ */
 contract Arbitrable{
     Arbitrator public arbitrator;
     
     modifier onlyArbitrator {require(msg.sender==address(arbitrator)); _;}
     
+    /** @dev To be raised when a dispute is created. The main purpose of this event is to let the arbitrator know the meaning ruling IDs.
+     *  @param _arbitrator The arbitrator of the contract.
+     *  @param _disputeID ID of the dispute in the Arbitrator contract.
+     *  @param _rulingOptions Map ruling IDs to short description of the ruling. Note that ruling IDs start a 1. For example ["Send funds to buyer", "Send funds to seller"], means that ruling 1 will make the contract send funds to the buyer and 2 to the seller.
+     */
+    event Dispute(Arbitrator indexed _arbitrator, uint indexed _disputeID, string[] _rulingOptions);
+    
+    /** @dev Constructor. Choose the arbitrator.
+     *  @param _arbitrator The arbitrator of the contract. Can be an Arbitrator or an external account.
+     */
+    function Arbitrable(Arbitrator _arbitrator) {
+        arbitrator=_arbitrator;
+    }
+    
     /** @dev Give a ruling for a dispute. Must be call by the arbitrator.
-     *  The arbitrable smart contract determines the consequences of this ruling.
      *  @param _disputeID ID of the dispute in the Arbitrator contract.
      *  @param _ruling Ruling given by the arbitrator. Note that 0 is reserved for "Not able/wanting to make a decision".
      */
-    function rule(uint _disputeID, uint _ruling) onlyArbitrator {}
+    function rule(uint _disputeID, uint _ruling) onlyArbitrator {
+        executeRuling(_disputeID,_ruling);
+    }
+    
+    
+    /** @dev Execute a ruling of a dispute.
+     *  @param _disputeID ID of the dispute in the Arbitrator contract.
+     *  @param _ruling Ruling given by the arbitrator. Note that 0 is reserved for "Not able/wanting to make a decision".
+     */
+    function executeRuling(uint _disputeID, uint _ruling) internal;
 }
 
-
+/** @title Arbitrator
+ *  Arbitrator abstract contract.
+ *  When developing arbitrator contracts we need to:
+ *  -Define the functions for dispute creation (createDispute) and appeal (appeal). Don't forget to store the arbitrated contract and the disputeID (which should be unique, use nbDisputes).
+ *  -Define the functions for cost display (arbitrationCost and appealCost).
+ *  -Allow giving rulings. For this a function must call arbitrable.rule(disputeID,ruling).
+ *  
+ */
 contract Arbitrator{
+    
+    uint public nbDisputes; // Number of disputes. Usefull for disputeID.
     
     modifier requireArbitrationFee(bytes _extraData) {require(msg.value>=arbitrationCost(_extraData)); _;}
     modifier requireAppealFee(uint _disputeID, bytes _extraData) {require(msg.value>=appealCost(_disputeID, _extraData)); _;}
@@ -51,8 +91,3 @@ contract Arbitrator{
      */
     function appealCost(uint _disputeID, bytes _extraData) constant returns(uint fee);
 }
-
-
-
-
-
