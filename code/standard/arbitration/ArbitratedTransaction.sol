180a16f6ecaf545c4ed75f88d14f788313526f17
Clément Lesaege <clemage@live.fr>
2017-09-23 20:27:32 +0200

    Made it inherit from TwoPartyArbitrable
    Lot of the logic was transfered to TwoPartyArbitrable

--- code/standard/arbitration/ArbitratedTransaction.sol
+++ code/standard/arbitration/ArbitratedTransaction.sol
@@ -6,162 +6,60 @@
 
 
 pragma solidity ^0.4.16;
-import "./ArbitrationStandard.sol";
-
+import "./TwoPartyArbitrable.sol";
 
 /** @title Arbitrated Transaction
  *  This is a a contract for an arbitrated transaction which can be reversed by the arbitrator.
  *  This can be used for buying goods, services and for paying freelancers.
- *  TODO: Implement appeals.
+ *  Party A is the payer. Party B is the payee.
  */
-contract ArbitratedTransaction is Arbitrable {
-    uint public timeout;
-    address public payer;
-    address public payee;
-    uint public payerFee; // Fee paid by the payer.
-    uint public payeeFee; // Fee paid by the payee.
-    bytes public arbitratorExtraData;
-    uint public lastInteraction; // Last interaction for the dispute procedure.
-    uint public disputeID;
-    enum Status {NoDispute, WaitingPayee, WaitingPayer, DisputeCreated}
-    Status public status;
+ contract ArbitratedTransaction is TwoPartyArbitrable {
+    string constant RULING_OPTIONS = "Reimburse partyA;Pay partyB";
     
-    uint8 constant AMOUNT_OF_CHOICES = 2;
-    uint8 constant REIMBURSE_PAYER = 1;
-    uint8 constant PAY_PAYEE = 1;
-    string constant RULING_OPTIONS = "Reimburse Payer;Pay Payee";
+    uint public amount; // Amount sent by party A.
     
-    modifier onlyPayer{ require(msg.sender==payer); _; }
-    modifier onlyPayee{ require(msg.sender==payee); _; }
     
-    enum Party {Payer, Payee}
-    
-    /** @dev Indicate that a party has to pay a fee or would otherwise be considered as loosing.
-     *  @param _party The party who has to pay.
-     */
-    event HasToPayFee(Party _party);
-    
-    /** @dev Constructor. Choose the arbitrator.
+    /** @dev Constructor. Choose the arbitrator. Should be called by party A (the payer).
      *  @param _arbitrator The arbitrator of the contract.
      *  @param _timeout Time after which a party automatically loose a dispute.
-     *  @param _payee The recipient of the transaction.
+     *  @param _partyB The recipient of the transaction.
      *  @param _arbitratorExtraData Extra data for the arbitrator.
      */
-    function ArbitratedTransaction(Arbitrator _arbitrator, uint _timeout, address _payee, bytes _arbitratorExtraData) Arbitrable(_arbitrator) payable {
-        timeout=_timeout;
-        payer=msg.sender;
-        payee=_payee;
-        arbitratorExtraData=_arbitratorExtraData;
+    function ArbitratedTransaction(Arbitrator _arbitrator, uint _timeout, address _partyB, bytes _arbitratorExtraData) TwoPartyArbitrable(_arbitrator,_timeout,_partyB,_arbitratorExtraData) payable {
+        amount+=msg.value;
     }
-    
-    /** @dev Pay the payee. To be called when the good is delivered or the service rendered.
+
+    /** @dev Pay the party B. To be called when the good is delivered or the service rendered.
      */
-    function pay() onlyPayer {
-        payee.transfer(this.balance);
+    function pay() onlyPartyA {
+        partyB.transfer(amount);
+        amount=0;
     }
     
-    /** @dev Reimburse the payer. To be called if the good or service can't be fully provided.
-     *  @param _amount Amount to reimburse in wei. If this is above the total amount the contract has, reimburse everything.
+    /** @dev Reimburse party A. To be called if the good or service can't be fully provided.
+     *  @param _amountReimbursed Amount to reimburse in wei.
      */
-    function reimburse(uint _amount) onlyPayee {
-        payer.transfer(_amount>this.balance ? this.balance : _amount);
+    function reimburse(uint _amountReimbursed) onlyPartyB {
+        require(_amountReimbursed<=amount);
+        partyA.transfer(_amountReimbursed);
+        amount-=_amountReimbursed;
     }
     
-    
-    /** @dev Pay the arbitration fee to raise a dispute. To be called by the payer. UNTRUSTED.
-     *  Note that the arbitrator can have createDispute throw, which will make this function throw and therefore lead to a party being timed-out.
-     *  This is not a vulnerability as the arbitrator can rule in favor of one party anyway.
-     */
-    function payArbitrationFeeByPayer() payable onlyPayer {
-        uint arbitrationCost=arbitrator.arbitrationCost(arbitratorExtraData);
-        require(msg.value+payerFee == arbitrationCost); // Require that the total pay at least the arbitration cost.
-        require(status<Status.DisputeCreated); // Make sure a dispute has not been created yet.
-        
-        lastInteraction=now;
-        if (payeeFee < arbitrationCost) { // The payee still has to pay. This can also happens if he has paid, but arbitrationCost has increased.
-            status=Status.WaitingPayee;
-            HasToPayFee(Party.Payee);
-        } else { // The payee has also paid the fee. We create the dispute
-            payerFee+=msg.value-arbitrationCost; // Note that the right part will undeflow, but the result will still be correct.    
-            payeeFee-=arbitrationCost;
-            raiseDispute(arbitrationCost);
-        }
-    }
-    
-    
-    /** @dev Pay the arbitration fee to raise a dispute. To be called by the payee. UNTRUSTED.
-     *  Note that this functio mirror payArbitrationFeePayer.
-     */
-    function payArbitrationFeeByPayee() payable onlyPayee {
-        uint arbitrationCost=arbitrator.arbitrationCost(arbitratorExtraData);
-        require(msg.value+payeeFee == arbitrationCost); // Require that the total pay at least the arbitration cost.
-        require(status<Status.DisputeCreated); // Make sure a dispute has not been created yet.
-        
-        lastInteraction=now;
-        if (payerFee < arbitrationCost) { // The payer still has to pay. This can also happens if he has paid, but arbitrationCost has increased.
-            status=Status.WaitingPayer;
-            HasToPayFee(Party.Payer);
-        } else { // The payer has also paid the fee. We create the dispute
-            payeeFee+=msg.value-arbitrationCost; // Note that the right part will undeflow, but the result will still be correct.    
-            payerFee-=arbitrationCost;
-            raiseDispute(arbitrationCost);
-        }
-    }
-    
-    /** @dev Create a dispute.
-     *  @param _arbitrationCost Amount to pay the arbitrator.
-     */
-    function raiseDispute(uint _arbitrationCost) internal {
-        status=Status.DisputeCreated;
-        disputeID=arbitrator.createDispute.value(_arbitrationCost)(AMOUNT_OF_CHOICES,arbitratorExtraData);
-        Dispute(arbitrator,disputeID,RULING_OPTIONS);
-    }
-    
-    /** @dev Reimburse payer if payee fails to pay the fee.
-     */
-    function timeOutByPayer() onlyPayer {
-        require(status==Status.WaitingPayee);
-        require(now>=lastInteraction+timeout);
-        
-        executeRuling(disputeID,REIMBURSE_PAYER);
-    }
-    
-    /** @dev Pay payee if payer fails to pay the fee.
-     */
-    function timeOutByPayee() onlyPayer {
-        require(status==Status.WaitingPayer);
-        require(now>=lastInteraction+timeout);
-        
-        executeRuling(disputeID,PAY_PAYEE);
-    }
-    
-    /** @dev Submit a reference to evidence. EVENT.
-     *  @param _evidence A link to an evidence using its URI.
-     */
-    function submitEvidence(string _evidence) {
-        require(status>=Status.DisputeCreated);
-        require(msg.sender==payer || msg.sender==payee);
-        Evidence(arbitrator,disputeID,msg.sender,_evidence);
-    }
-    
-    /** @dev Execute a ruling of a dispute.
+    /** @dev Execute a ruling of a dispute. It reimburse the fee to the winning party.
+     *  This need to be extended by contract inheriting from it.
      *  @param _disputeID ID of the dispute in the Arbitrator contract.
-     *  @param _ruling Ruling given by the arbitrator. 1 : Reimburse the payer. 2 : Pay the payee.
+     *  @param _ruling Ruling given by the arbitrator. 1 : Reimburse the partyA. 2 : Pay the partyB.
      */
     function executeRuling(uint _disputeID, uint _ruling) internal {
-        require(_disputeID==disputeID);
-        require(_ruling<=AMOUNT_OF_CHOICES);
-        
-        // Give all the value present in this contract. Note that this means arbitration fees are reimbursed for the winning party.
-        if (_ruling == REIMBURSE_PAYER)
-            payer.transfer(this.balance);
-        else if (_ruling == PAY_PAYEE)
-            payee.transfer(this.balance);
+        super.executeRuling(_disputeID,_ruling);
+        if (_ruling==PARTY_A_WINS)
+            partyA.send(amount);
+        else if (_ruling==PARTY_B_WINS)
+            partyB.send(amount);
+            
+        amount=0;
     }
     
-    /** @dev Allow the payer to paid additional money without recreating a contract.
-     */
-    function () payable {}
-
     
-}
+ }
+ 
