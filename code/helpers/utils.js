5cb63bdb29585d03f15e3212bdc9e1c420fcf547
clesaege <clemage@live.fr>
2017-09-28 01:48:41 +0200

    Added tests

---
+++ code/helpers/utils.js
@@ -0,0 +1,49 @@
+module.exports = {
+
+ expectThrow: async promise => {
+
+   try {
+
+     await promise
+
+   } catch (error) {
+
+     const invalidJump = error.message.search('invalid JUMP') >= 0
+
+     const invalidOpcode = error.message.search('invalid opcode') >= 0
+
+     const outOfGas = error.message.search('out of gas') >= 0
+
+     assert(invalidJump || invalidOpcode || outOfGas, "Expected throw, got '" + error + "' instead")
+
+     return
+
+   }
+
+   assert.fail('Expected throw not received')
+
+ },
+
+ waitForMined: tx => {
+
+   return new Promise((resolve, reject) => {
+
+     let setIntervalId = setInterval(() => web3.eth.getTransactionReceipt(tx, (err, receipt) => {
+
+       if (err) reject(err.message)
+
+       if (receipt) {
+
+         clearInterval(setIntervalId)
+
+         resolve(receipt)
+
+       }
+
+     }), 1000)
+
+   })
+
+ }
+
+}
