45090487c062db7530b03374aba278bb38c45fe3
clesaege <clemage@live.fr>
2017-10-22 17:09:38 +0200

    Improved comments.

--- contracts/standard/rng/BlockhashRNG.sol
+++ contracts/standard/rng/BlockhashRNG.sol
@@ -41,14 +41,16 @@ contract BlockHashRNG is RNG {
             return RN;
     }
     
-    
+    /** @dev Save the random number for this blockhash and give the reward to the caller.
+     *  @param _block Block the random number is linked to.
+     */
     function saveRN(uint _block) public {
         if (block.blockhash(_block)!=0x0) {
             uint rewardToSend=reward[_block];
             reward[_block]=0;
             randomNumber[_block]=uint(block.blockhash(_block));
             
-            msg.sender.send(rewardToSend); // Note that the use of send is on purpose as we don't want to block in case the msg.sender has a fallback issue
+            msg.sender.send(rewardToSend); // Note that the use of send is on purpose as we don't want to block in case the msg.sender has a fallback issue.
             
         }  
     }
