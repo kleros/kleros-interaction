b1c55ebb550befc71de9b4e70e5b690f96c7732e
Clément Lesaege <clemage@live.fr>
2017-11-21 22:51:45 +0100

    Update contract name

--- contracts/standard/whitelist/AddressWhitelistInterface.sol
+++ contracts/standard/whitelist/AddressWhitelistInterface.sol
@@ -9,7 +9,7 @@ pragma solidity ^0.4.15;
  *  @title Address Whitelist Interface
  *  This is a Whitelist interface for addresses.
  */
-interface WhitelistInterface{
+interface AddressWhitelistInterface{
     /** @dev Return true is the address is allowed.
      *  @param _value The address we want to know if allowed.
      *  @return allowed True if the address is allowed, false otherwize.
