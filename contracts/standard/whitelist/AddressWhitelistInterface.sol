0c76a74274bd19529ce853a8ad23a1df7bc23bbc
Clément Lesaege <clemage@live.fr>
2017-11-21 22:50:46 +0100

    Create AddressWhitelistInterface.sol

---
+++ contracts/standard/whitelist/AddressWhitelistInterface.sol
@@ -0,0 +1,18 @@
+/**
+ *  @title Address Whitelist Interface
+ *  @author Clément Lesaege - <clement@lesaege.com>
+ */
+
+pragma solidity ^0.4.15;
+
+/**
+ *  @title Address Whitelist Interface
+ *  This is a Whitelist interface for addresses.
+ */
+interface WhitelistInterface{
+    /** @dev Return true is the address is allowed.
+     *  @param _value The address we want to know if allowed.
+     *  @return allowed True if the address is allowed, false otherwize.
+     */
+    function isAllowed(address _value) public returns (bool allowed);
+}
