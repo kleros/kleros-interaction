8faa623a2e0eed597e97f827498f91694c2e8646
Clément Lesaege <clemage@live.fr>
2017-11-21 22:48:48 +0100

    Update WhitelistInterface.sol

--- contracts/standard/whitelist/WhitelistInterface.sol
+++ contracts/standard/whitelist/WhitelistInterface.sol
@@ -1,13 +1,13 @@
 /**
- *  @title WhitelistInterface
+ *  @title Whitelist Interface
  *  @author Clément Lesaege - <clement@lesaege.com>
  */
 
 pragma solidity ^0.4.15;
 
 /**
- *  @title WhitelistInterface
- *  This is a White
+ *  @title Whitelist Interface
+ *  This is a Whitelist for arbitrary values. The values can be cast to the required types. If the type is less than 32 bytes, you may want to use a particular type of whitelist.
  */
 interface WhitelistInterface{
     /** @dev Return true is the value is allowed.
