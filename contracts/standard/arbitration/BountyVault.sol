406a61f472f7756d61f38d5f030c37030442a1da
Clément Lesaege <clemage@live.fr>
2017-12-18 01:16:04 +0100

    added comments in TODO

--- contracts/standard/arbitration/BountyVault.sol
+++ contracts/standard/arbitration/BountyVault.sol
@@ -1,4 +1,5 @@
 // TODO: Create a contract which holds the monthly bounty amount.
+// Team can fund the vault.
 // Team can award points to contributors.
 // Contributors can make claims to get points (and put a deposit). This leads to disputes with the team.
 // People can challenge points of contributors (and put a deposit). If they win their challenges, points are removed.
