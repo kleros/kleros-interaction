691a8c9c902f79b03022ab12c81669b82e304c14
Clément Lesaege <clemage@live.fr>
2017-12-25 00:40:50 +0100

    Add ERC20 Version of Minime.

---
+++ contracts/standard/arbitration/ArbitrableTokens/MiniMeTokenERC20.sol
@@ -0,0 +1,28 @@
+ /**
+ *  @title Mini Me Token ERC20
+ *  Overwrite the MiniMeToken to make it ERC20.
+ *  This is required because the base token reverts when approve is used with the non zero value while allowed is non zero (which forbidden by the standard, see https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md).
+ *  @author Clément Lesaege - <clement@lesaege.com>
+ *  Bug Bounties: This code hasn't undertaken a bug bounty program yet.
+ */
+
+pragma solidity ^0.4.15;
+
+import "minimetoken/contracts/MiniMeToken.sol";
+
+contract MiniMeTokenERC20 is MiniMeToken {
+    
+    /** @notice `msg.sender` approves `_spender` to spend `_amount` tokens on its behalf.
+      * This is a ERC20 compliant version.
+      * @param _spender The address of the account able to transfer the tokens
+      * @param _amount The amount of tokens to be approved for transfer
+      * @return True if the approval was successful
+      */
+    function approve(address _spender, uint256 _amount) public returns (bool success) {
+        require(transfersEnabled);
+        // Alerts the token controller of the approve function call
+        if (isContract(controller)) {
+            require(TokenController(controller).onApprove(msg.sender, _spender, _amount));
+        }
+    }
+}
