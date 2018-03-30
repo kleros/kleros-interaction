6f8f1893dd30d4814b2a231c824279a46d44be6a
epiqueras <epiquerass@gmail.com>
2018-03-30 09:45:31 -0700

    feat(arbitrator-court-and-proxy): more precise address types

--- contracts/standard/arbitration/ArbitratorCourt.sol
+++ contracts/standard/arbitration/ArbitratorCourt.sol
@@ -12,7 +12,7 @@ contract ArbitratorCourt is Arbitrator {
 
     struct Court {
         string name;
-        address _address;
+        Arbitrator _address;
     }
 
     /* Events */
@@ -24,7 +24,7 @@ contract ArbitratorCourt is Arbitrator {
      * @param _nextParentName The next `parent`'s name.
      * @param _nextParentAddress The next `parent`'s address.
      */
-    event OnParentChange(string _prevParentName, address _prevParentAddress, string _nextParentName, address _nextParentAddress);
+    event OnParentChange(string _prevParentName, Arbitrator _prevParentAddress, string _nextParentName, Arbitrator _nextParentAddress);
 
     /**
      * @notice Called whenever a sub court is added.
@@ -32,7 +32,7 @@ contract ArbitratorCourt is Arbitrator {
      * @param _subCourtName The subcourt's name.
      * @param _subCourtAddress The subcourt's address.
      */
-    event OnSubCourtAdd(uint256 _subCourtIndex, string _subCourtName, address _subCourtAddress);
+    event OnSubCourtAdd(uint256 _subCourtIndex, string _subCourtName, Arbitrator _subCourtAddress);
 
     /**
      * @notice Called whenever a sub court is removed.
@@ -40,7 +40,7 @@ contract ArbitratorCourt is Arbitrator {
      * @param _subCourtName The subcourt's name.
      * @param _subCourtAddress The subcourt's address.
      */
-    event OnSubCourtRemove(uint256 _subCourtIndex, string _subCourtName, address _subCourtAddress);
+    event OnSubCourtRemove(uint256 _subCourtIndex, string _subCourtName, Arbitrator _subCourtAddress);
 
     /* Storage */
 
@@ -68,7 +68,7 @@ contract ArbitratorCourt is Arbitrator {
      * @param _nextParentName The next `parent`'s name.
      * @param _nextParentAddress The next `parent`'s address.
      */
-    function setParent(string _nextParentName, address _nextParentAddress) external onlyOwner {
+    function setParent(string _nextParentName, Arbitrator _nextParentAddress) external onlyOwner {
         // Emit event before overwriting `parent`
         emit OnParentChange(parent.name, parent._address, _nextParentName, _nextParentAddress);
 
@@ -81,7 +81,7 @@ contract ArbitratorCourt is Arbitrator {
      * @param _subCourtName The subcourt's name.
      * @param _subCourtAddress The subcourt's address.
      */
-    function addSubCourt(string _subCourtName, address _subCourtAddress) external onlyOwner {
+    function addSubCourt(string _subCourtName, Arbitrator _subCourtAddress) external onlyOwner {
         uint256 _index = subCourts.push(Court({ name: _subCourtName, _address: _subCourtAddress })) - 1;
         emit OnSubCourtAdd(_index, _subCourtName, _subCourtAddress);
     }
