f6fc28380bbd5c51148201fc57413fbba76bea60
Clément Lesaege <clemage@live.fr>
2018-07-24 15:03:57 +0100

    Update comments which were blacklist specific

--- contracts/standard/permission/ArbitrablePermissionList.sol
+++ contracts/standard/permission/ArbitrablePermissionList.sol
@@ -12,8 +12,7 @@ import "./PermissionInterface.sol";
 /**
  *  @title Arbitrable Permission List
  *  @dev This is an arbitrator curated registry. Anyone can post an item with a deposit. If no one complains within a defined time period, the item is added to the registry.
- *  Someone can complain and also post a deposit, if, someone does, a dispute is created. The winner of the dispute gets the deposit of the other party and the item is added or removed accordingly.
- *  During the time of the dispute, the item is shown as blacklisted unless it already won a previous dispute. This follows the philosophy that it is better to show the user a warning about a potentially harmless listing than to take the risk of the user being scammed or exposed to inappropriate content without warning.
+ *  Anyone can complain and also post a deposit. If someone does, a dispute is created. The winner of the dispute gets the deposit of the other party and the item is added or removed accordingly.
  *  To make a request, parties have to deposit a stake and the arbitration fees. If the arbitration fees change between the submitter's payment and the challenger's payment, a part of the submitter stake can be used as an arbitration fee deposit.
  *  In case the arbitrator refuses to rule, the item is put in the initial absent status and the balance is split equally between parties.
  */
