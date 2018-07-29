2c2fa8fc43add470488fe5be38706fc90f4e0733
Clément Lesaege <clemage@live.fr>
2018-07-29 16:45:31 +0000

    Prevent resetting number after 256 blocks and pay even if already set

--- contracts/standard/rng/BlockhashRNG.sol
+++ contracts/standard/rng/BlockhashRNG.sol
@@ -45,13 +45,12 @@ contract BlockHashRNG is RNG {
      *  @param _block Block the random number is linked to.
      */
     function saveRN(uint _block) public {
-        if (blockhash(_block) != 0x0) {
-            uint rewardToSend = reward[_block];
-            reward[_block] = 0;
+        if (blockhash(_block) != 0x0)
             randomNumber[_block] = uint(blockhash(_block));
-
+        if (randomNumber[_block] != 0) { // If the number is set.
+            reward[_block] = 0;
+            uint rewardToSend = reward[_block];
             msg.sender.send(rewardToSend); // Note that the use of send is on purpose as we don't want to block in case msg.sender has a fallback issue.
-
         }
     }
 
--- contracts/standard/rng/BlockhashRNGFallback.sol
+++ contracts/standard/rng/BlockhashRNGFallback.sol
@@ -24,15 +24,17 @@ contract BlockHashRNGFallback is BlockHashRNG {
      */
     function saveRN(uint _block) public {
         if (_block<block.number) {
+            if (randomNumber[_block] == 0) { // If the random number is not already set.
+                if (blockhash(_block)!=0x0) // Normal case.
+                    randomNumber[_block]=uint(blockhash(_block));
+                else // The contract was not called in time. Fallback to returning previous blockhash.
+                    randomNumber[_block]=uint(blockhash(block.number-1));
+            }            
+        }
+        if (randomNumber[_block] != 0) { // If the random number is set.
             uint rewardToSend=reward[_block];
             reward[_block]=0;
-            if (blockhash(_block)!=0x0) // Normal case.
-                randomNumber[_block]=uint(blockhash(_block));
-            else // The contract was not called in time. Fallback to returning previous blockhash.
-                randomNumber[_block]=uint(blockhash(block.number-1));
-            
             msg.sender.send(rewardToSend); // Note that the use of send is on purpose as we don't want to block in case the msg.sender has a fallback issue.
-            
         }
     }
     
