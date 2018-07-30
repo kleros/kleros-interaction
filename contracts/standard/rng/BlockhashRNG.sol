0f8edcaf6b2dfa81edf9746946c3751d3b0aada1
Clément Lesaege <clemage@live.fr>
2018-07-30 13:24:25 +0100

    Update BlockhashRNG.sol

--- contracts/standard/rng/BlockhashRNG.sol
+++ contracts/standard/rng/BlockhashRNG.sol
@@ -48,8 +48,8 @@ contract BlockHashRNG is RNG {
         if (blockhash(_block) != 0x0)
             randomNumber[_block] = uint(blockhash(_block));
         if (randomNumber[_block] != 0) { // If the number is set.
-            reward[_block] = 0;
             uint rewardToSend = reward[_block];
+            reward[_block] = 0;
             msg.sender.send(rewardToSend); // Note that the use of send is on purpose as we don't want to block in case msg.sender has a fallback issue.
         }
     }
