e932d5ffe3e27cc1445f50e7dd4d7f9e0f20212f
epiqueras <epiquerass@gmail.com>
2018-09-15 16:56:30 -0500

    feat(backed-up-arbitrator): add time out

--- contracts/standard/arbitration/BackedUpArbitrator.sol
+++ contracts/standard/arbitration/BackedUpArbitrator.sol
@@ -11,15 +11,18 @@ contract BackedUpArbitrator is CentralizedArbitrator, Arbitrable {
     /* Storage */
 
     Arbitrator public backUp;
+    uint public timeOut;
     mapping(uint => uint) public creationTimes;
 
     /* Constructor */
 
     /** @dev Constructs the BackedUpArbitrator contract.
      *  @param _backUp The back up arbitrator.
+     *  @param _timeOut The time that needs to pass for a pending dispute to be appealable.
      */
-    constructor(Arbitrator _backUp) public {
+    constructor(Arbitrator _backUp, uint _timeOut) public {
         backUp = _backUp;
+        timeOut = _timeOut;
     }
 
     /* External */
@@ -29,6 +32,13 @@ contract BackedUpArbitrator is CentralizedArbitrator, Arbitrable {
      */
     function changeBackUp(Arbitrator _backUp) external onlyOwner {
         backUp = _backUp;
+    }
+
+    /** @dev Changes the time out.
+     *  @param _timeOut The new time out.
+     */
+    function changeTimeOut(uint _timeOut) external onlyOwner {
+        timeOut = _timeOut;
     }
 
     /* External Views */
