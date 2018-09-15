b2e95ef714d48f5c96b1aa1bcaa1eb07022fd3d3
epiqueras <epiquerass@gmail.com>
2018-09-15 16:42:27 -0500

    feat(backed-up-arbitrator): create contract

---
+++ contracts/standard/arbitration/BackedUpArbitrator.sol
@@ -0,0 +1,71 @@
+pragma solidity ^0.4.24;
+
+import "./CentralizedArbitrator.sol";
+
+/**
+ *  @title BackedUpArbitrator
+ *  @author Enrique Piqueras - <epiquerass@gmail.com>
+ *  @dev A centralized arbitrator that can be appealed when not responsive.
+ */
+contract BackedUpArbitrator is CentralizedArbitrator, Arbitrable {
+    /* Enums */
+
+
+
+    /* Structs */
+
+
+
+    /* Events */
+
+
+
+    /* Storage */
+
+
+
+    /* Modifiers */
+
+
+
+    /* Constructor */
+
+
+
+    /* Fallback */
+
+
+
+    /* External */
+
+
+
+    /* External Views */
+
+
+
+    /* Public */
+
+
+
+    /* Public Views */
+
+
+
+    /* Internal */
+
+
+
+    /* Internal Views */
+
+
+
+    /* Private */
+
+
+
+    /* Private Views */
+
+
+
+}
