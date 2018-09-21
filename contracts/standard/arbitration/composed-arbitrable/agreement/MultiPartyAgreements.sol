8719535f05b737a7617bc6623921492d91f3ee08
epiqueras <epiquerass@gmail.com>
2018-09-21 14:08:46 -0500

    feat(multi-party-agreements): create contract

--- contracts/standard/arbitration/composed-arbitrable/agreement/MultiPartyAgreements.sol
+++ contracts/standard/arbitration/composed-arbitrable/agreement/MultiPartyAgreements.sol
@@ -1,1 +1,69 @@
-pragma solidity ^0.4.24;
+pragma solidity ^0.4.15;
+
+/**
+ *  @title MultiPartyAgreements
+ *  @author Enrique Piqueras - <epiquerass@gmail.com>
+ *  @dev Agreement part of a composed arbitrable contract. Handles multi-party, multi-choice dispute agreements.
+ */
+contract MultiPartyAgreements {
+    /* Enums */
+
+
+
+    /* Structs */
+
+
+
+    /* Events */
+
+
+
+    /* Storage */
+
+
+
+    /* Modifiers */
+
+
+
+    /* Constructor */
+
+
+
+    /* Fallback */
+
+
+
+    /* External */
+
+
+
+    /* External Views */
+
+
+
+    /* Public */
+
+
+
+    /* Public Views */
+
+
+
+    /* Internal */
+
+
+
+    /* Internal Views */
+
+
+
+    /* Private */
+
+
+
+    /* Private Views */
+
+
+
+}
