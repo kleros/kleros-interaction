6b1694291e84bdaa45179c69073623cfb94dae1f
epiqueras <epiquerass@gmail.com>
2018-09-21 14:41:06 -0500

    feat(multi-party-agreements): add structs

--- contracts/standard/arbitration/composed-arbitrable/agreement/MultiPartyAgreements.sol
+++ contracts/standard/arbitration/composed-arbitrable/agreement/MultiPartyAgreements.sol
@@ -1,4 +1,6 @@
-pragma solidity ^0.4.15;
+pragma solidity ^0.4.24;
+
+import "../../Arbitrator.sol";
 
 /**
  *  @title MultiPartyAgreements
@@ -6,13 +8,20 @@ pragma solidity ^0.4.15;
  *  @dev Agreement part of a composed arbitrable contract. Handles multi-party, multi-choice dispute agreements.
  */
 contract MultiPartyAgreements {
-    /* Enums */
-
-
-
     /* Structs */
 
-
+    struct Agreement {
+        address creator; // The agreement's creator.
+        address[] parties; // The involved parties.
+        uint value; // The value held by the agreement.
+        uint numberOfChoices; // The number of choices in a dispute arising from the agreement.
+        bytes extraData; // The extra data in a dispute arising from the agreement.
+        uint arbitrationFeesWaitingTime; // The maximum time to wait for arbitration fees.
+        uint appealFeesWaitingTime; // The maximum time to wait for appeal fees.
+        Arbitrator arbitrator; // The arbitrator to use in a dispute arising from the agreement.
+        uint disputeID; // The agreement's dispute ID, if disputed.
+        bool disputed; // Wether the agreement is disputed or not.
+    }
 
     /* Events */
 
