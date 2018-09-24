763f550415e76be03e9de018c443f4b29301a967
epiqueras <epiquerass@gmail.com>
2018-09-24 09:29:50 -0500

    feat(multi-party-insurable-fees): storage and constructor

--- contracts/standard/arbitration/composed-arbitrable/fee/MultiPartyInsurableFees.sol
+++ contracts/standard/arbitration/composed-arbitrable/fee/MultiPartyInsurableFees.sol
@@ -1,1 +1,70 @@
 pragma solidity ^0.4.24;
+
+import "../agreement/MultiPartyAgreements.sol";
+
+/**
+ *  @title MultiPartyInsurableFees
+ *  @author Enrique Piqueras - <epiquerass@gmail.com>
+ *  @dev Fee part of a composed arbitrable contract. Handles crowdinsured arbitration and appeal fees.
+ */
+contract MultiPartyInsurableFees is MultiPartyAgreements {
+    /* Structs */
+
+    struct Contribution {
+        address _address; // The address that contributed.
+        uint value; // The value contributed.
+    }
+    struct PaidFees {
+        uint[] stake; // The stake required in each round.
+        uint[] totalValue; // The current held value in each round.
+        uint[2][] totalContributedPerSide; // The total amount contributed per side in each round.
+        mapping(address => Contribution)[] contributions; // The contributions in each round.
+    }
+
+    /* Storage */
+
+    uint public stake;
+    mapping(bytes32 => PaidFees) internal paidFees;
+
+    /* Constructor */
+
+    /** @dev Constructs the `MultiPartyInsurableFees` contract.
+     *  @param _stake The stake parameter for sharing fees.
+     */
+    constructor(uint _stake) public {
+        stake = _stake;
+    }
+
+    /* External */
+
+
+
+    /* External Views */
+
+
+
+    /* Public */
+
+
+
+    /* Public Views */
+
+
+
+    /* Internal */
+
+
+
+    /* Internal Views */
+
+
+
+    /* Private */
+
+
+
+    /* Private Views */
+
+
+
+}
