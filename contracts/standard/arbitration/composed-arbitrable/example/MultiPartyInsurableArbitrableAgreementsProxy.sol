5d6aa7a6bb24a50ace8792bf0e654252a12e0953
epiqueras <epiquerass@gmail.com>
2018-09-25 13:06:41 -0500

    feat(composed-arbitrable): implement example proxy

--- contracts/standard/arbitration/composed-arbitrable/example/MultiPartyInsurableArbitrableAgreementsProxy.sol
+++ contracts/standard/arbitration/composed-arbitrable/example/MultiPartyInsurableArbitrableAgreementsProxy.sol
@@ -1,1 +1,30 @@
 pragma solidity ^0.4.24;
+
+import "../MultiPartyInsurableArbitrableAgreementsBase.sol";
+
+import "./ComposedArbitrableProxyUser.sol";
+
+/**
+ *  @title MultiPartyInsurableArbitrableAgreementsProxy
+ *  @author Enrique Piqueras - <epiquerass@gmail.com>
+ *  @notice Proxy implementation of `MultiPartyInsurableArbitrableAgreementsBase`.
+ */
+contract MultiPartyInsurableArbitrableAgreementsProxy is MultiPartyInsurableArbitrableAgreementsBase {
+    /* Constructor */
+
+    /** @dev Constructs the `MultiPartyInsurableArbitrableAgreementsProxy` contract.
+     *  @param _feeGovernor The fee governor of this contract.
+     *  @param _stake The stake parameter for sharing fees.
+     */
+    constructor(address _feeGovernor, uint _stake) public MultiPartyInsurableArbitrableAgreementsBase(_feeGovernor, _stake) {}
+
+    /* Internal */
+
+    /** @dev Executes the ruling on the specified agreement.
+     *  @param _agreementID The ID of the agreement.
+     *  @param _ruling The ruling.
+     */
+    function executeAgreementRuling(bytes32 _agreementID, uint _ruling) internal {
+        ComposedArbitrableProxyUser(agreements[_agreementID].creator).executeAgreementRuling(_agreementID, _ruling);
+    }
+}
