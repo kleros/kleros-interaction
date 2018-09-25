da88a2bb18b42a532ef4094a997a692b38582aa0
epiqueras <epiquerass@gmail.com>
2018-09-25 12:53:00 -0500

    feat(composed-arbitrable): implement first base

--- contracts/standard/arbitration/composed-arbitrable/MultiPartyInsurableArbitrableAgreementsBase.sol
+++ contracts/standard/arbitration/composed-arbitrable/MultiPartyInsurableArbitrableAgreementsBase.sol
@@ -1,1 +1,20 @@
 pragma solidity ^0.4.24;
+
+import "./agreement/MultiPartyAgreements.sol";
+import "./fee/MultiPartyInsurableFees.sol";
+import "./evidence/MultiPartyEvidence.sol";
+
+/**
+ *  @title MultiPartyInsurableArbitrableAgreementsBase
+ *  @author Enrique Piqueras - <epiquerass@gmail.com>
+ *  @dev Base composed arbitrable contract. Handles multi-party, multi-choice dispute agreements with party evidence and crowdinsured arbitration and appeal fees.
+ */
+contract MultiPartyInsurableArbitrableAgreementsBase is MultiPartyAgreements, MultiPartyInsurableFees, MultiPartyEvidence {
+    /* Constructor */
+
+    /** @dev Constructs the `MultiPartyInsurableArbitrableAgreementsBase` contract.
+     *  @param _feeGovernor The fee governor of this contract.
+     *  @param _stake The stake parameter for sharing fees.
+     */
+    constructor(address _feeGovernor, uint _stake) public MultiPartyInsurableFees(_feeGovernor, _stake) {}
+}
