fe0d33b42f17d0ef837d524fdc4ac2f4b3479dcc
epiqueras <epiquerass@gmail.com>
2018-09-25 13:06:41 -0500

    feat(composed-arbitrable): implement example proxy

---
+++ contracts/standard/arbitration/composed-arbitrable/example/ComposedArbitrableProxyUser.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.4.24;
+
+/**
+ *  @title ComposedArbitrableProxyUser
+ *  @author Enrique Piqueras - <epiquerass@gmail.com>
+ *  @dev A base contract for contracts that use a composed arbitrable proxy.
+ */
+contract ComposedArbitrableProxyUser {
+    /* Public */
+
+    /** @dev Executes the ruling on the specified agreement.
+     *  @param _agreementID The ID of the agreement.
+     *  @param _ruling The ruling.
+     */
+    function executeAgreementRuling(bytes32 _agreementID, uint _ruling) public;
+}
