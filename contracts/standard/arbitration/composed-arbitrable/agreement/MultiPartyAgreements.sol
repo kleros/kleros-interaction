0f7545948429ee2d2329b48857eb0e2e6beb40de
epiqueras <epiquerass@gmail.com>
2018-09-25 20:11:46 -0500

    feat(composed-arbitrable): add agreement execution and remove value

--- contracts/standard/arbitration/composed-arbitrable/agreement/MultiPartyAgreements.sol
+++ contracts/standard/arbitration/composed-arbitrable/agreement/MultiPartyAgreements.sol
@@ -13,7 +13,6 @@ contract MultiPartyAgreements is Arbitrable {
     struct Agreement {
         address creator; // The agreement's creator.
         address[] parties; // The involved parties.
-        uint value; // The value held by the agreement.
         uint numberOfChoices; // The number of choices in a dispute arising from the agreement.
         bytes extraData; // The extra data in a dispute arising from the agreement.
         uint arbitrationFeesWaitingTime; // The maximum time to wait for arbitration fees.
@@ -22,6 +21,7 @@ contract MultiPartyAgreements is Arbitrable {
         bool disputed; // Wether the agreement is disputed or not.
         bool appealed; // Wether the agreement's dispute has been appealed or not.
         uint ruling; // The final ruling for the agreement's dispute.
+        bool executed; // Wether the agreement has been executed or not.
     }
 
     /* Storage */
@@ -48,13 +48,12 @@ contract MultiPartyAgreements is Arbitrable {
         bytes _extraData,
         uint _arbitrationFeesWaitingTime,
         Arbitrator _arbitrator
-    ) public payable {
+    ) public {
         require(agreements[_agreementID].creator == address(0), "The supplied agreement ID is already being used.");
         require(_parties.length <= 10, "There cannot be more than 10 parties.");
         agreements[_agreementID] = Agreement({
             creator: msg.sender,
             parties: _parties,
-            value: msg.value,
             numberOfChoices: _numberOfChoices,
             extraData: _extraData,
             arbitrationFeesWaitingTime: _arbitrationFeesWaitingTime,
@@ -62,7 +61,8 @@ contract MultiPartyAgreements is Arbitrable {
             disputeID: 0,
             disputed: false,
             appealed: false,
-            ruling: 0
+            ruling: 0,
+            executed: false
         });
         emit MetaEvidence(uint(_agreementID), _metaEvidence);
     }
