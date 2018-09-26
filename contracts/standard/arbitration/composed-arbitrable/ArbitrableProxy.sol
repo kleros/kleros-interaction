c206aa0cf96abe010122e11ec7f094fdc33f76de
epiqueras <epiquerass@gmail.com>
2018-09-25 21:18:49 -0500

    feat(composed-arbitrable): standardize arbitrable proxies

---
+++ contracts/standard/arbitration/composed-arbitrable/ArbitrableProxy.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.24;
+
+import "../Arbitrator.sol";
+
+/**
+ *  @title ArbitrableProxy
+ *  @author Enrique Piqueras - <epiquerass@gmail.com>
+ *  @dev Base contract for arbitrable proxies.
+ */
+contract ArbitrableProxy {
+    /* External */
+
+    /** @dev Creates an agreement.
+     *  @param _agreementID The ID of the agreement.
+     *  @param _metaEvidence The meta evidence of the agreement.
+     *  @param _parties The `parties` value of the agreement.
+     *  @param _numberOfChoices The `numberOfChoices` value of the agreement.
+     *  @param _extraData The `extraData` value of the agreement.
+     *  @param _arbitrationFeesWaitingTime The `arbitrationFeesWaitingTime` value of the agreement.
+     *  @param _arbitrator The `arbitrator` value of the agreement.
+     */
+    function createAgreement(
+        bytes32 _agreementID,
+        string _metaEvidence,
+        address[] _parties,
+        uint _numberOfChoices,
+        bytes _extraData,
+        uint _arbitrationFeesWaitingTime,
+        Arbitrator _arbitrator) external;
+}
