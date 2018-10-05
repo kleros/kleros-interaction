001ba5ebd9d0b05f64d23e3dd3cd7f3a636dd51a
epiqueras <epiquerass@gmail.com>
2018-10-05 16:27:35 -0500

    feat(arbitration): simplest arbitrable example

---
+++ contracts/standard/arbitration/TwoPartyArbitrableEscrowPayment.sol
@@ -0,0 +1,67 @@
+pragma solidity ^0.4.24;
+
+import "./Arbitrable.sol";
+
+contract TwoPartyArbitrableEscrowPayment is Arbitrable {
+    address public sender;
+    address public receiver;
+    uint public value;
+    bytes public extraData;
+    Arbitrator public arbitrator;
+    uint public disputeID;
+    bool public disputed;
+    bool public appealed;
+    bool public executed;
+    uint public createdAt;
+    uint public timeOut;
+
+    modifier onlySender{require(msg.sender == sender, "Can only be called by the sender."); _;}
+    modifier onlyReceiver{require(msg.sender == receiver, "Can only be called by the receiver."); _;}
+    modifier onlySenderOrReceiver{
+        require(msg.sender == sender || msg.sender == receiver, "Can only be called by the sender or the receiver.");
+        _;
+    }
+
+    constructor(address _receiver, bytes _extraData, Arbitrator _arbitrator, uint _timeOut, string _metaEvidence) public payable {
+        sender = msg.sender;
+        receiver = _receiver;
+        value = msg.value;
+        extraData = _extraData;
+        arbitrator = _arbitrator;
+        createdAt = now;
+        timeOut = _timeOut;
+        emit MetaEvidence(0, _metaEvidence);
+    }
+
+    function raiseDispute() public payable onlySenderOrReceiver {
+        disputeID = arbitrator.createDispute.value(msg.value)(2, extraData);
+        emit Dispute(arbitrator, disputeID, 0);
+    }
+
+    function submitEvidence(string _evidence) public onlySenderOrReceiver {
+        require(disputed, "The payment has to be disputed.");
+        require(!appealed, "The payment can not be appealed.");
+        emit Evidence(arbitrator, disputeID, msg.sender, _evidence);
+    }
+
+    function appeal() public payable onlySenderOrReceiver {
+        arbitrator.appeal.value(msg.value)(disputeID, extraData);
+        if (!appealed) appealed = true;
+    }
+
+    function executePayment() public onlySenderOrReceiver {
+        require(now - createdAt > timeOut, "The timeout time has not passed yet.");
+        require(!executed, "The payment was already executed.");
+        executed = true;
+        receiver.send(value);
+    }
+
+    function executeRuling(uint _disputeID, uint _ruling) internal {
+        require(_disputeID == disputeID, "Wrong dispute ID.");
+        require(!executed, "The payment was already executed.");
+        executed = true;
+        if (_ruling == 2) receiver.send(value);
+        else sender.send(value);
+        emit Ruling(arbitrator, disputeID, _ruling);
+    }
+}
