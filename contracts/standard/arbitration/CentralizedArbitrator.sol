21ec443036799ad3aec9326d95bfe236d575fa3b
Ferit Tunçer <ferit@cryptolab.net>
2018-11-16 15:49:01 +0300

    style(centralized-arbitrator): linting fixes in doxygen comments

--- contracts/standard/arbitration/CentralizedArbitrator.sol
+++ contracts/standard/arbitration/CentralizedArbitrator.sol
@@ -1,9 +1,9 @@
 /**
- * @authors: [@clesaege, @n1c01a5, @epiqueras, @ferittuncer]
- * @reviewers: []
- * @auditors: []
- * @bounties: []
- * @deployments: []
+ *  @authors: [@clesaege, @n1c01a5, @epiqueras, @ferittuncer]
+ *  @reviewers: []
+ *  @auditors: []
+ *  @bounties: []
+ *  @deployments: []
  */
 
 pragma solidity ^0.4.15;
@@ -32,42 +32,42 @@ contract CentralizedArbitrator is Arbitrator {
     DisputeStruct[] public disputes;
 
     /** @dev Constructor. Set the initial arbitration price.
-    *  @param _arbitrationPrice Amount to be paid for arbitration.
-    */
+     *  @param _arbitrationPrice Amount to be paid for arbitration.
+     */
     constructor(uint _arbitrationPrice) public {
         arbitrationPrice = _arbitrationPrice;
     }
 
     /** @dev Set the arbitration price. Only callable by the owner.
-    *  @param _arbitrationPrice Amount to be paid for arbitration.
-    */
+     *  @param _arbitrationPrice Amount to be paid for arbitration.
+     */
     function setArbitrationPrice(uint _arbitrationPrice) public onlyOwner {
         arbitrationPrice = _arbitrationPrice;
     }
 
     /** @dev Cost of arbitration. Accessor to arbitrationPrice.
-    *  @param _extraData Not used by this contract.
-    *  @return fee Amount to be paid.
-    */
+     *  @param _extraData Not used by this contract.
+     *  @return fee Amount to be paid.
+     */
     function arbitrationCost(bytes _extraData) public view returns(uint fee) {
         return arbitrationPrice;
     }
 
     /** @dev Cost of appeal. Since it is not possible, it's a high value which can never be paid.
-    *  @param _disputeID ID of the dispute to be appealed. Not used by this contract.
-    *  @param _extraData Not used by this contract.
-    *  @return fee Amount to be paid.
-    */
+     *  @param _disputeID ID of the dispute to be appealed. Not used by this contract.
+     *  @param _extraData Not used by this contract.
+     *  @return fee Amount to be paid.
+     */
     function appealCost(uint _disputeID, bytes _extraData) public view returns(uint fee) {
         return NOT_PAYABLE_VALUE;
     }
 
     /** @dev Create a dispute. Must be called by the arbitrable contract.
-    *  Must be paid at least arbitrationCost().
-    *  @param _choices Amount of choices the arbitrator can make in this dispute. When ruling ruling<=choices.
-    *  @param _extraData Can be used to give additional info on the dispute to be created.
-    *  @return disputeID ID of the dispute created.
-    */
+     *  Must be paid at least arbitrationCost().
+     *  @param _choices Amount of choices the arbitrator can make in this dispute. When ruling ruling<=choices.
+     *  @param _extraData Can be used to give additional info on the dispute to be created.
+     *  @return disputeID ID of the dispute created.
+     */
     function createDispute(uint _choices, bytes _extraData) public payable returns(uint disputeID)  {
         super.createDispute(_choices,_extraData);
         disputeID = disputes.push(DisputeStruct({
@@ -82,9 +82,9 @@ contract CentralizedArbitrator is Arbitrator {
     }
 
     /** @dev Give a ruling. UNTRUSTED.
-    *  @param _disputeID ID of the dispute to rule.
-    *  @param _ruling Ruling given by the arbitrator. Note that 0 means "Not able/wanting to make a decision".
-    */
+     *  @param _disputeID ID of the dispute to rule.
+     *  @param _ruling Ruling given by the arbitrator. Note that 0 means "Not able/wanting to make a decision".
+     */
     function _giveRuling(uint _disputeID, uint _ruling) internal {
         DisputeStruct dispute = disputes[_disputeID];
         require(_ruling <= dispute.choices, "Invalid ruling.");
@@ -98,25 +98,25 @@ contract CentralizedArbitrator is Arbitrator {
     }
 
     /** @dev Give a ruling. UNTRUSTED.
-    *  @param _disputeID ID of the dispute to rule.
-    *  @param _ruling Ruling given by the arbitrator. Note that 0 means "Not able/wanting to make a decision".
-    */
+     *  @param _disputeID ID of the dispute to rule.
+     *  @param _ruling Ruling given by the arbitrator. Note that 0 means "Not able/wanting to make a decision".
+     */
     function giveRuling(uint _disputeID, uint _ruling) public onlyOwner {
         return _giveRuling(_disputeID, _ruling);
     }
 
     /** @dev Return the status of a dispute.
-    *  @param _disputeID ID of the dispute to rule.
-    *  @return status The status of the dispute.
-    */
+     *  @param _disputeID ID of the dispute to rule.
+     *  @return status The status of the dispute.
+     */
     function disputeStatus(uint _disputeID) public view returns(DisputeStatus status) {
         return disputes[_disputeID].status;
     }
 
     /** @dev Return the ruling of a dispute.
-    *  @param _disputeID ID of the dispute to rule.
-    *  @return ruling The ruling which would or has been given.
-    */
+     *  @param _disputeID ID of the dispute to rule.
+     *  @return ruling The ruling which would or has been given.
+     */
     function currentRuling(uint _disputeID) public view returns(uint ruling) {
         return disputes[_disputeID].ruling;
     }
