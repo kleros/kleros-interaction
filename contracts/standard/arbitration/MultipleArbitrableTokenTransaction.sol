59880510fcfc495f70a0072afcbab90e02443071
unknownunknown1 <time_arrow@bk.ru>
2019-06-07 16:01:11 +1000

    docs(MultipleArbitrableTokenTransaction): removed asterisk

--- contracts/standard/arbitration/MultipleArbitrableTokenTransaction.sol
+++ contracts/standard/arbitration/MultipleArbitrableTokenTransaction.sol
@@ -1,6 +1,6 @@
 /**
  *  @authors: [@n1c01a5, @hellwolf, @satello]
- *  @reviewers: [@ferittuncer*, @unknownunknown1*, @mtsalenc]
+ *  @reviewers: [@ferittuncer*, @unknownunknown1, @mtsalenc]
  *  @auditors: []
  *  @bounties: []
  *  @deployments: []
