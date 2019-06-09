2c2f3c59a954bf6fbb24254df5e6de744c5df19f
Clément Lesaege <clemage@live.fr>
2019-06-09 18:24:53 +0100

    fix(comments)

--- contracts/standard/permission/ArbitrableTokenList.sol
+++ contracts/standard/permission/ArbitrableTokenList.sol
@@ -433,7 +433,7 @@ contract ArbitrableTokenList is PermissionInterface, Arbitrable {
      *  @param _tokenID The token ID with funds to be withdrawn.
      *  @param _request The request from which to withdraw contributions.
      *  @param _cursor The round from where to start withdrawing.
-     *  @param _count The number of rounds to iterate. If set to 0 or a value larger than the number of rounds, iterates until the last round.
+     *  @param _count Rounds greater or equal to this value won't be withdrawn. If set to 0 or a value larger than the number of rounds, iterates until the last round.
      */
     function batchRoundWithdraw(address _beneficiary, bytes32 _tokenID, uint _request, uint _cursor, uint _count) public {
         Token storage token = tokens[_tokenID];
@@ -446,7 +446,7 @@ contract ArbitrableTokenList is PermissionInterface, Arbitrable {
      *  @param _beneficiary The address that made contributions to the request.
      *  @param _tokenID The token ID with funds to be withdrawn.
      *  @param _cursor The request from which to start withdrawing.
-     *  @param _count The number of requests to iterate. If set to 0 or a value larger than the number of request, iterates until the last request.
+     *  @param _count Requests greater or equal to this value won't be withdrawn. If set to 0 or a value larger than the number of request, iterates until the last request.
      *  @param _roundCursor The round of each request from where to start withdrawing.
      *  @param _roundCount The number of rounds to iterate on each request. If set to 0 or a value larger than the number of rounds a request has, iteration for that request will stop at the last round.
      */
