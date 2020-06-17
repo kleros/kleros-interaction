260bee88572309370095c48424b9a71acea16afc
unknownunknown1 <time_arrow@bk.ru>
2020-06-17 18:31:51 +1000

    refactor(LinguoToken.sol): follow check-effect-interaction pattern

--- contracts/standard/arbitration/LinguoToken.sol
+++ contracts/standard/arbitration/LinguoToken.sol
@@ -260,15 +260,15 @@ contract LinguoToken is Arbitrable {
         task.parties[uint(Party.Translator)] = msg.sender;
         task.status = Status.Assigned;
 
-        uint remainder = task.maxPrice.subCap(price);
-        require(task.token.transfer(task.requester, remainder), "Could not transfer tokens to the requester.");
         // Update requester's deposit since we reimbursed him the difference between maximal and actual price.
         task.requesterDeposit = price;
         task.sumDeposit += translatorDeposit;
 
-        remainder = msg.value - translatorDeposit;
+        uint remainder = msg.value - translatorDeposit;
         msg.sender.send(remainder);
 
+        remainder = task.maxPrice.subCap(price);
+        require(task.token.transfer(task.requester, remainder), "Could not transfer tokens to the requester.");
         emit TaskAssigned(_taskID, msg.sender, price, now);
     }
 
@@ -300,8 +300,8 @@ contract LinguoToken is Arbitrable {
         task.requesterDeposit = 0;
         task.sumDeposit = 0;
         // Requester gets his deposit back and also the deposit of the translator, if there was one.  Note that sumDeposit can't contain challenger's deposit until the task is in DisputeCreated status.
-        require(task.token.transfer(task.requester, requesterDeposit), "The token transfer was unsuccessful.");
         task.requester.send(sumDeposit);
+        require(task.token.transfer(task.requester, requesterDeposit), "The token transfer was unsuccessful.");
 
         emit TaskResolved(_taskID, "requester-reimbursed", now);
     }
@@ -320,9 +320,9 @@ contract LinguoToken is Arbitrable {
         uint sumDeposit = task.sumDeposit;
         task.requesterDeposit = 0;
         task.sumDeposit = 0;
-        require(task.token.transfer(translator, requesterDeposit), "The token transfer was unsuccessful.");
         translator.send(sumDeposit);
-        
+        require(task.token.transfer(translator, requesterDeposit), "The token transfer was unsuccessful.");
+
         emit TaskResolved(_taskID, "translation-accepted", now);
     }
 
@@ -519,17 +519,19 @@ contract LinguoToken is Arbitrable {
         task.sumDeposit = 0;
 
         if(_ruling == uint(Party.None)){
-            require(task.token.transfer(task.requester, task.requesterDeposit), "Could not transfer tokens to requester.");
             // The value of sumDeposit is split among parties in this case. If the sum is uneven the value of 1 wei can be burnt.
             sumDeposit = sumDeposit / 2;
             task.parties[uint(Party.Translator)].send(sumDeposit);
             task.parties[uint(Party.Challenger)].send(sumDeposit);
-        } else if (_ruling == uint(Party.Translator)) {
-            require(task.token.transfer(task.parties[uint(Party.Translator)], requesterDeposit), "Could not transfer tokens to translator.");
-            task.parties[uint(Party.Translator)].send(sumDeposit);
-        } else {
+            // When using a token of the advanced standard (ERC777 etc) the requester can revert this function and prevent fee reimbursement.
+            // This is not an issue however, because "0" ruling is an edge case.
             require(task.token.transfer(task.requester, requesterDeposit), "Could not transfer tokens to requester.");
+        } else if (_ruling == uint(Party.Translator)) {
+            task.parties[uint(Party.Translator)].send(sumDeposit);
+            require(task.token.transfer(task.parties[uint(Party.Translator)], requesterDeposit), "Could not transfer tokens to translator.");
+        } else {
             task.parties[uint(Party.Challenger)].send(sumDeposit);
+            require(task.token.transfer(task.requester, requesterDeposit), "Could not transfer tokens to requester.");
         }
 
         emit TaskResolved(taskID, "dispute-settled", now);
