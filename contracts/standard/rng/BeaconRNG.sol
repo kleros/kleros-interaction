eaa70dcfa6fd62ee617459db1b09d04c89555e95
unknownunknown1 <time_arrow@bk.ru>
2022-09-01 02:32:22 +1000

    fix(RNG): store the RN value and combine BeaconRNG

--- contracts/standard/rng/BeaconRNG.sol
+++ contracts/standard/rng/BeaconRNG.sol
@@ -1,6 +1,6 @@
  /**
- *  @authors: [@shalzz]
- *  @reviewers: [@jaybuidl]
+ *  @authors: [@shalzz, @unknownunknown1]
+ *  @reviewers: [@jaybuidl*, @geaxed*]
  *  @auditors: []
  *  @bounties: []
  *  @deployments: []
@@ -14,9 +14,21 @@ import "./RNG.sol";
  *  @title Random Number Generator using beacon chain random opcode
  */
 contract BeaconRNG is RNG {
+    
+    uint public constant LOOKAHEAD = 132; // Number of blocks that has to pass before obtaining the random number. 4 epochs + 4 slots, according to EIP-4399.
+
+    RNG public blockhashRNG; // Address of blockhashRNG to fall back on.
+    mapping (uint => uint) public randomNumber; // randomNumber[block] is the random number for this requested block, 0 otherwise.
+
+    /** @dev Constructor.
+     * @param _blockhashRNG The blockhash RNG deployed contract address.
+     */
+    constructor(RNG _blockhashRNG) public {
+        blockhashRNG = _blockhashRNG;
+    }
 
     /**
-     * @dev Since we don't really need to incentivise requesting the beacon chain randomness,
+     * @dev Since we don't really need to incentivize requesting the beacon chain randomness,
      * this is a stub implementation required for backwards compatibility with the
      * RNG interface.
      * @notice All the ETH sent here will be lost forever.
@@ -24,15 +36,41 @@ contract BeaconRNG is RNG {
      */
     function contribute(uint _block) public payable {}
 
-
-    /** @dev Return the random number from the PoS randomness beacon.
-     *  @param _block Block the random number is linked to.
-     *  @return RN Random Number. If the PoS upgrade defined by EIP-3675
-     *          has not yet executed 0 instead.
+    /**
+     * @dev Request a random number.
+     * @dev Since the beacon chain randomness is not related to a block
+     * we can call ahead its getRN function to check if the PoS merge has happened or not.
+     *  
+     * @param _block Block linked to the request.
      */
-    function getRN(uint _block) public returns (uint RN) {
-        if (block.difficulty <= 2**64)
+    function requestRN(uint _block) public payable {
+        // Use the old RNG pre-Merge.
+        if (block.difficulty <= 2**64) {
+            blockhashRNG.contribute(_block);
+        } else {
+            contribute(_block);
+        }
+    }
+
+    /**
+     * @dev Get the random number.
+     * @param _block Block the random number is linked to.
+     * @return RN Random Number. If the number is not ready or has not been required 0 instead.
+     */
+    function getRN(uint _block) public returns (uint) {
+        // if beacon chain randomness is zero
+        // fallback to blockhash RNG
+        if (block.difficulty <= 2**64) {
+            return blockhashRNG.getRN(_block);
+        } else if (block.number < _block + LOOKAHEAD) {
+            // Beacon chain returns the random number, but sufficient number of blocks hasn't been mined yet.
+            // In this case signal to the court that RN isn't ready.
             return 0;
-        return block.difficulty;
+        } else {
+            if (randomNumber[_block] == 0) {
+                randomNumber[_block] = block.difficulty;
+            }
+            return randomNumber[_block]; 
+        }
     }
 }
