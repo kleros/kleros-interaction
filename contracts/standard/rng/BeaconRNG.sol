19bda596fd7cecd078f8362a6ca3fa4bb75d06e2
Clément Lesaege <clemage@live.fr>
2022-09-06 18:29:55 +0000

    name(specify we are using the fallbackRNG)

--- contracts/standard/rng/BeaconRNG.sol
+++ contracts/standard/rng/BeaconRNG.sol
@@ -18,13 +18,13 @@ contract BeaconRNG {
     uint public constant LOOKAHEAD = 132; // Number of blocks that has to pass before obtaining the random number. 4 epochs + 4 slots, according to EIP-4399.
     uint public constant ERROR = 32; // Number of blocks after which the lookahead gets reset, so eligible blocks after lookahead don't go long distance, to avoid a possiblity for manipulation.
 
-    RNG public blockhashRNG; // Address of blockhashRNG to fall back on.
+    RNG public blockhashRNGFallback; // Address of blockhashRNGFallback to fall back on.
 
     /** @dev Constructor.
-     * @param _blockhashRNG The blockhash RNG deployed contract address.
+     * @param _blockhashRNGFallback The blockhash RNG deployed contract address.
      */
-    constructor(RNG _blockhashRNG) public {
-        blockhashRNG = _blockhashRNG;
+    constructor(RNG _blockhashRNGFallback) public {
+        blockhashRNGFallback = _blockhashRNGFallback;
     }
 
     /**
@@ -40,7 +40,7 @@ contract BeaconRNG {
     function getUncorrelatedRN(uint _block) public returns (uint) {
         // Pre-Merge.
         if (block.difficulty <= 2**64) {
-            uint baseRN = blockhashRNG.getRN(_block);
+            uint baseRN = blockhashRNGFallback.getRN(_block);
             if (baseRN == 0) {
                 return 0;
             } else {
